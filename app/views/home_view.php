7bab4e0aef842264f96df6a3c98e10be2ef1e6c8
pascalc <pascal.chevrel@free.fr>
2014-10-30 13:35:22 +0100

    wording nit

--- app/views/home_view.php
+++ app/views/home_view.php
@@ -9,7 +9,7 @@
 <?php foreach($android_locales as $lang): ?>
 <tr>
     <th><?=$lang?></th>
-    <td><a href='./?locale=<?=$lang?>'>View</a></td>
+    <td><a href='./?locale=<?=$lang?>'>Show</a></td>
     <td><a href='./?locale=<?=$lang?>&amp;output=html'>HTML</a></td>
     <td><a href='./?locale=<?=$lang?>&amp;output=json'>Json</a></td>
 </tr>
