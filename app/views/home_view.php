f7ed15da04bacacc4a3ec3c6a3f3410d8bac107a
pascalc <pascal.chevrel@free.fr>
2014-10-30 13:07:26 +0100

    update home view with html source column

--- app/views/home_view.php
+++ app/views/home_view.php
@@ -1,17 +1,15 @@
 <table>
 <tr>
     <th>Locale</th>
-    <th>View</th>
-<!--     <th>Raw HTML</th>
-    <th>Raw Text</th>
- -->    <th>Json</th>
+    <th>General View</th>
+    <th>Description Raw HTML</th>
+    <th>Description Json</th>
 </tr>
 <?php foreach($android_locales as $lang): ?>
 <tr>
     <th><?=$lang?></th>
     <td><a href='./?locale=<?=$lang?>'>View</a></td>
-    <!-- <td></td>
-    <td></td> -->
+    <td><a href='./?locale=<?=$lang?>&amp;output=html'>HTML</a></td>
     <td><a href='./?locale=<?=$lang?>&amp;output=json'>Json</a></td>
 </tr>
 <?php endforeach; ?>
