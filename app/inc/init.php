667427d799a37425c4eb623380b5f52b8ce64623
pascalc <pascal.chevrel@free.fr>
2014-11-02 22:55:21 +0100

    Be explicit with the use if UTF8 everywhere

--- app/inc/init.php
+++ app/inc/init.php
@@ -1,5 +1,8 @@
 <?php
 namespace Play;
 
+// We always work with UTF8 encoding (needed for PHP < 5.6)
+mb_internal_encoding('UTF-8');
+
 require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/../settings/config.php';
