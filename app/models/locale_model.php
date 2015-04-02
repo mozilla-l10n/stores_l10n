63ebf4bbc6772b0a16693ea066b8bcaaec664164
Pascal Chevrel <pascal.chevrel@free.fr>
2015-04-02 16:45:03 +0200

    Add limits for Google Play title and short description lengths, display on preview page when these limits are passed.

--- app/models/locale_model.php
+++ app/models/locale_model.php
@@ -6,10 +6,18 @@ $translate = new Translate($locale, 'description_page.lang');
 // Include the current template
 require_once APP_ROOT . '/templates/' . array_values($current_template)[0];
 
-$description_length = mb_strlen(trim(strip_tags($description($translate))));
+$get_length = function ($string) {
+    return mb_strlen(trim(strip_tags($string)));
+};
 
-if ($description_length < 4000) {
-    $warning = $description_length . ' characters';
-} else {
-    $warning = '<strong style="color:red">' . $description_length . ' characters, too long. Limit is 4000.</strong>';
-}
+$set_limit = function ($limit, $length) {
+    if ($length < $limit) {
+        return $length . ' characters';
+    }
+
+    return '<strong style="color:red">' . $length . " characters, too long. Limit is {$limit}.</strong>";
+};
+
+$listing_warning    = $set_limit(4000, $get_length($description($translate)));
+$title_warning      = $set_limit(30, $get_length($app_title($translate)));
+$short_desc_warning = $set_limit(80, $get_length($short_desc($translate)));
--- app/views/locale_view.php
+++ app/views/locale_view.php
@@ -1,5 +1,11 @@
+
+<h2><?= $app_title($translate) ?> &mdash; <?= $title_warning ?></h2>
+
+<h2><em><?= $short_desc($translate) ?> &mdash; <?= $short_desc_warning ?></em></h2>
+
+
 <h3>Top 10 Features for Google Play Store</h3>
 <?= $top_10($translate) ?>
-<h3>Google Play Listing Copy &mdash; <?= $warning ?></h3>
 
-<?= $description($translate) ?>
+<h3>Google Play Listing Copy &mdash; <?= $listing_warning ?></h3>
+<pre><?= $description($translate) ?></pre>
