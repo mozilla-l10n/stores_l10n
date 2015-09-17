5219756a5913d0dbd02acf35ca4704e3d3f92f26
Pascal Chevrel <pascal.chevrel@free.fr>
2015-09-17 15:58:38 +0200

    Update upstream Langchecker libraries

--- app/classes/Langchecker/DotLangParser.php
+++ app/classes/Langchecker/DotLangParser.php
@@ -76,100 +76,100 @@ class DotLangParser
             return self::$parsed_files[$path];
         }
 
+        // Set up some basic data to avoid further checks later
+        $dotlang_data = [
+            'activated' => false,
+            'strings'   => [],
+            'errors'    => [
+                'ignoredstrings' => [],
+            ],
+        ];
+
         $file_content = self::getFile($path);
+        // Stop if the file is empty
+        if (! $file_content) {
+            return $dotlang_data;
+        }
 
-        $dotlang_data = [];
+        for ($i = 0, $lines = count($file_content); $i < $lines; $i++) {
+            $current_line = $file_content[$i];
 
-        // In case the file is missing we set an empty array for strings
-        $dotlang_data['strings'] = [];
+            // Ignore comments and metadata if we're only interested in strings
+            if (! self::$extract_metadata && Utils::startsWith($current_line, '#')) {
+                continue;
+            }
 
-        // Make sure there's always an activation status to avoid controls later
-        $dotlang_data['activated'] = false;
+            /* First line may contain an activation status.
+             * In Bedrock tags are read with regexp "^## (\w+) ##", so
+             * trailing spaces can be safely ignored
+             */
+            if ($i == 0 && rtrim($current_line) == '## active ##') {
+                $dotlang_data['activated'] = true;
+                continue;
+            }
 
-        if ($file_content !== false) {
-            // First pass: extract strings and metadata (tags, active status) relevant for all locales.
-            $lines = count($file_content);
-            foreach ($file_content as $i => $current_line) {
-                if (! self::$extract_metadata && Utils::startsWith($current_line, '#')) {
-                    continue;
-                }
+            /* Other tags like ## promo_news ##, but not meta data
+             * These tags are always before strings.
+             */
+            if (empty($dotlang_data['strings'])
+                && Utils::startsWith($current_line, '##')
+                && ! Utils::startsWith($current_line, self::getMetaTags())) {
+                $dotlang_data['tags'][] = trim(str_replace('##', '', $current_line));
+                continue;
+            }
 
-                /* First line may contain an activation status
-                 * Tags are read with regexp "^## (\w+) ##", so trailing spaces can be ignored
-                 */
-                if ($i == 0 && rtrim($current_line) == '## active ##') {
-                    $dotlang_data['activated'] = true;
-                    continue;
-                }
+            if ($i < $lines - 1) {
+                $next_line = $file_content[$i + 1];
+            } else {
+                $next_line = '';
+            }
 
-                // Get file description
-                if (empty($dotlang_data['strings']) // File description is always before strings
-                    && Utils::startsWith($current_line, '## NOTE:')) {
-                    $dotlang_data['filedescription'][] = trim(Utils::leftStrip($current_line, '## NOTE:'));
-                    continue;
-                }
+            if (! empty($next_line) && Utils::startsWith($current_line, ';')) {
+                // Source strings start with ";". I have a reference string followed by something
+                $reference = Utils::leftStrip($current_line, ';');
+                $translation = trim($next_line);
 
-                // Get file stage URL
-                if (! isset($dotlang_data['url'])
-                    && empty($dotlang_data['strings']) // URL is always before strings
-                    && Utils::startsWith($current_line, '## URL:')) {
-                    $dotlang_data['url'] = trim(Utils::leftStrip($current_line, '## URL:'));
-                    continue;
-                }
-
-                // Other tags like ## promo_news ##, but not meta data
-                if (empty($dotlang_data['strings']) // Other tags are  always before strings
-                    && Utils::startsWith($current_line, '##')
-                    && ! Utils::startsWith($current_line, self::getMetaTags())) {
-                    $dotlang_data['tags'][] = trim(str_replace('##', '', $current_line));
-                    continue;
-                }
-
-                if ($i < $lines - 1) {
-                    $next_line = $file_content[$i + 1];
-                } else {
-                    $next_line = '';
-                }
-
-                if (! empty($next_line) && Utils::startsWith($current_line, ';')) {
-                    // Source strings start with ";". I have a reference string followed by something
-                    $reference = Utils::leftStrip($current_line, ';');
-                    $translation = trim($next_line);
-
-                    if (isset($dotlang_data['strings'][$reference]) && $reference_locale) {
-                        /* String is already stored, it's a duplicated string. If it's the reference
-                         * locale I save the string ID to issue a warning where necessary.
-                         */
-                        $dotlang_data['duplicates'][] = $reference;
-                    }
-
-                    // If we don't extract metadata, we shouldn't test for #
-                    $markers = self::$extract_metadata
-                        ? [';', '#']
-                        : [';'];
-
-                    // We test the most common scenario first: reference string followed by a translation
-                    if (! Utils::startsWith($translation, $markers)) {
-                        // Store the translation
-                        $dotlang_data['strings'][$reference] = $translation;
-                        continue;
-                    }
-
-                    /* Empty translation: what I'm reading as translation is either the next reference string
-                     * or the next meta tag (comment, tag binding). I consider this string untranslated.
+                if (isset($dotlang_data['strings'][$reference]) && $reference_locale) {
+                    /* String is already stored, it's a duplicated string. If it's the reference
+                     * locale I save the string ID to issue a warning where necessary.
                      */
-                    $dotlang_data['strings'][$reference] = $reference;
+                    $dotlang_data['duplicates'][] = $reference;
                 }
+
+                // If we don't extract metadata, we shouldn't test for #
+                $markers = self::$extract_metadata
+                    ? [';', '#']
+                    : [';'];
+
+                // We test the most common scenario first: reference string followed by a translation
+                if (! Utils::startsWith($translation, $markers)) {
+                    // Store the translation
+                    $dotlang_data['strings'][$reference] = $translation;
+                    // We can ignore checks for line i+1, since we already know it's the translation just stored
+                    $i++;
+                    continue;
+                }
+
+                /* Empty translation: what I'm reading as translation is either the next reference string
+                 * or the next meta tag (comment, tag binding). I consider this string untranslated.
+                 */
+                $dotlang_data['strings'][$reference] = $reference;
+                continue;
             }
 
-            if ($reference_locale) {
-                // Second pass: extract only metadata (comments, tag bindings) for reference locale.
-                if (self::$extract_metadata) {
-                    $dotlang_data = array_merge($dotlang_data, self::extractReferenceMetaData($file_content));
-                }
-                // Store reference locale data statically because it is used multiple times
-                self::$parsed_files[$path] = $dotlang_data;
+            if (trim($current_line) != '' && ! Utils::startsWith($current_line, '#')) {
+                // If I reach this point I have a stray line of text that won't be used. Report an error
+                array_push($dotlang_data['errors']['ignoredstrings'], $current_line);
             }
+        }
+
+        if ($reference_locale) {
+            // Second pass: extract only metadata (comments, tag bindings) for reference locale.
+            if (self::$extract_metadata) {
+                $dotlang_data = array_merge($dotlang_data, self::extractReferenceMetaData($file_content));
+            }
+            // Store reference locale data statically because it is used multiple times
+            self::$parsed_files[$path] = $dotlang_data;
         }
 
         return $dotlang_data;
@@ -204,13 +204,30 @@ class DotLangParser
     private static function extractReferenceMetaData($file_content)
     {
         $dotlang_data = [];
+        $parsed_strings = false;
         for ($i = 0, $lines = count($file_content); $i < $lines; $i++) {
             $current_line = $file_content[$i];
+
+            if (! $parsed_strings) {
+                // Get file descriptions (always before strings)
+                if (Utils::startsWith($current_line, '## NOTE:')) {
+                    $dotlang_data['filedescription'][] = trim(Utils::leftStrip($current_line, '## NOTE:'));
+                    continue;
+                }
+
+                // Get demo URL (always before strings)
+                if (Utils::startsWith($current_line, '## URL:')) {
+                    $dotlang_data['url'] = trim(Utils::leftStrip($current_line, '## URL:'));
+                    continue;
+                }
+            }
+
             if (Utils::startsWith($current_line, ';')) {
                 // I have a reference string
+                $parsed_strings = true;
                 $reference = Utils::leftStrip($current_line, ';');
                 $j = $i - 1;
-                while ($j > 0) {
+                while ($j >= 0) {
                     // Stop if I find a line not starting with #
                     if (! Utils::startsWith($file_content[$j], '#')) {
                         break;
