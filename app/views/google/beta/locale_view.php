86c5de099b1071816f3355115c3e14e85e676acf
Théo Chevalier <theo.chevalier11@gmail.com>
2016-03-09 20:12:13 +0100

    nit, removing contenteditable="true" from locale view

--- app/views/google/beta/locale_view.php
+++ app/views/google/beta/locale_view.php
@@ -2,7 +2,7 @@
 <h2><?= $app_title($translations) ?> &mdash; <?= $title_warning ?></h2>
 
 <h3>What’s new &mdash; <?= $whatsnew_warning ?></h3>
-<pre contenteditable="true"><?= $whatsnew($translations) ?></pre>
+<pre><?= $whatsnew($translations) ?></pre>
 
 <h2><em><?= $short_desc($translations) ?> &mdash; <?= $short_desc_warning ?></em></h2>
 
