fb74641552e32312f412100bf9ca79e2bda3fdfb
Francesco Lodolo (:flod) <flod@lodolo.net>
2016-12-24 10:07:19 +0100

    Use Accept-Language locale if not provided

--- app/controllers/locale_controller.php
+++ app/controllers/locale_controller.php
@@ -6,12 +6,22 @@ namespace Stores;
 $formats = ['html', 'show', 'status'];
 $components = explode('/', $url['path']);
 
-$request = [
-    'locale'  => isset($components[1]) ? $components[1] : null,
-    'store'   => isset($components[2]) ? $components[2] : null,
-    'channel' => isset($components[3]) ? $components[3] : null,
-    'output'  => isset($components[4]) ? $components[4] : null,
-];
+// If locale is missing from URL, temporarily set it as en-US
+if (count($components) == 3) {
+    $request = [
+        'locale'  => 'en-US',
+        'store'   => isset($components[1]) ? $components[1] : null,
+        'channel' => isset($components[2]) ? $components[2] : null,
+        'output'  => isset($components[3]) ? $components[3] : null,
+    ];
+} else {
+    $request = [
+        'locale'  => isset($components[1]) ? $components[1] : null,
+        'store'   => isset($components[2]) ? $components[2] : null,
+        'channel' => isset($components[3]) ? $components[3] : null,
+        'output'  => isset($components[4]) ? $components[4] : null,
+    ];
+}
 
 if (! in_array($request['store'], ['google', 'apple'])) {
     die('Unknown marketplace provider.');
@@ -22,14 +32,19 @@ if (! in_array($request['channel'], ['beta', 'release'])) {
 }
 
 if ($request['store'] == 'google') {
-    $locales = $project->getGoogleMozillaCommonLocales($request['channel']);
+    $supported_locales = $project->getGoogleMozillaCommonLocales($request['channel']);
 }
 
 if ($request['store'] == 'apple') {
-    $locales = $project->getAppleMozillaCommonLocales($request['channel']);
+    $supported_locales = $project->getAppleMozillaCommonLocales($request['channel']);
 }
 
-if (! in_array($request['locale'], $locales)) {
+// If not provided, try to get a better locale match with Accept-Language
+if (count($components) == 3) {
+    $request['locale'] = Utils::detectLocale($supported_locales);
+}
+
+if (! in_array($request['locale'], $supported_locales)) {
     die('Not a locale code supported by Firefox');
 }
 
