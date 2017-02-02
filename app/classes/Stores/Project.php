6ccbdad1b9e079841ad21c7024040583a173ebcd
Francesco Lodolo (:flod) <flodolo@mozilla.com>
2017-02-02 13:12:29 +0100

    Remove duplicate screenshots section from FxA release, Update list of Focus locales

--- app/classes/Stores/Project.php
+++ app/classes/Stores/Project.php
@@ -128,9 +128,12 @@ class Project
         ],
         'focus_ios' => [
             'release' => [
-                'cs', 'de', 'en-US', 'es-CL', 'es-ES', 'fr', 'hu', 'id', 'it',
-                'ja', 'kab', 'pl', 'pt-BR', 'ru', 'sk', 'sl', 'sv-SE', 'zh-CN',
-                'zh-TW',
+                'ar', 'az', 'br', 'cs', 'cy', 'de', 'dsb', 'en-US', 'eo',
+                'es-CL', 'es-ES', 'fa', 'fr', 'ga-IE', 'gd', 'he', 'hi-IN',
+                'hsb', 'hu', 'id', 'is', 'it', 'ja', 'kab', 'kk', 'kn', 'lo',
+                'my', 'nb-NO', 'nl', 'nn-NO', 'pl', 'pt-BR', 'pt-PT', 'ro',
+                'ru', 'ses', 'sk', 'sl', 'sq', 'sv-SE', 'th', 'tr', 'uk', 'uz',
+                'zh-CN', 'zh-TW',
             ],
         ],
     ];
--- app/templates/fx_android/release/listing_apr_2016.php
+++ app/templates/fx_android/release/listing_apr_2016.php
@@ -70,33 +70,6 @@ OUT;
 $screenshots = function ($translations) use ($_) {
     return <<<OUT
 <b>{$_('Fast. Smart. Yours')}</b>
-{$_('Upgrade to the power of Firefox and<br>join hundreds of millions of<br>Firefox users on desktop and mobile.')}
-
-<b>{$_('Fast. Smart. Yours')}</b>
-{$_('Upgrade to the power of Firefox and<br>join hundreds of millions of Firefox users')}
-
-<b>{$_('Fast. Smart. Yours')}</b>
-{$_('Upgrade to Firefox and join<br>hundreds of millions of Firefox users')}
-
-{$_('Announcing<br>Firefox for iOS<br>Download now!')}
-
-<b>{$_('Intuitive visual tab design')}</b>
-{$_('Quickly find and manage<br>your open browser tabs')}
-
-<b>{$_('Intelligent searches')}</b>
-{$_('Get accurate, predictive results <br>before you finish typing')}
-
-<b>{$_('Seamless browsing <br>with Sync')}</b>
-{$_('Instantly access your bookmarks, <br>history, open tabs and passwords <br>everywhere you use Firefox')}
-
-<b>{$_('Private Browsing')}</b>
-{$_('Make specific tabs private and browse<br>the Web without your history being saved')}
-OUT;
-};
-
-$screenshots = function ($translations) use ($_) {
-    return <<<OUT
-<b>{$_('Fast. Smart. Yours')}</b>
 {$_('Upgrade to Firefox and join<br>hundreds of millions of Firefox users')}
 
 <b>{$_('Intuitive visual tab design')}</b>
