6baee5326f6f4cd4e2fbd2bd298f6c7f4ce8edd2
Francesco Lodolo (:flod) <flodolo@mozilla.com>
2017-03-22 09:42:12 +0100

    Add direction to all localized content in views

--- app/views/fx_ios/release/locale_escaped_view.php
+++ app/views/fx_ios/release/locale_escaped_view.php
@@ -1,18 +1,19 @@
 <h1>Firefox for iOS Listing Copy (<?= $request['locale'] ?>)</h1>
+
 <h3>Title</h3>
-<pre contenteditable="true"><?= htmlspecialchars($app_title($translations)) ?></pre>
+<pre <?= $direction ?> contenteditable="true"><?= htmlspecialchars($app_title($translations)) ?></pre>
 
 <h3>Description</h3>
-<pre contenteditable="true"><?= strip_tags($description($translations)) ?></pre>
+<pre <?= $direction ?> contenteditable="true"><?= strip_tags($description($translations)) ?></pre>
 
 <h3>What’s new</h3>
-<pre contenteditable="true"><?= $whatsnew($translations) ?></pre>
+<pre <?= $direction ?> contenteditable="true"><?= $whatsnew($translations) ?></pre>
 
 <h3>Screenshots text</h3>
-<pre contenteditable="true" class="text-center"><?= br2nl($screenshots($translations)) ?></pre>
+<pre <?= $direction ?> contenteditable="true" class="text-center"><?= br2nl($screenshots($translations)) ?></pre>
 
 <h3>Keywords &mdash; <?= $keywords_warning ?></h3>
-<pre contenteditable="true"><?= htmlspecialchars($keywords($translations)) ?></pre>
+<pre <?= $direction ?> contenteditable="true"><?= htmlspecialchars($keywords($translations)) ?></pre>
 
 <h3>Other</h3>
-<pre contenteditable="true"><?= htmlspecialchars($other($translations)) ?></pre>
+<pre <?= $direction ?> contenteditable="true"><?= htmlspecialchars($other($translations)) ?></pre>
--- app/views/fx_ios/release/locale_view.php
+++ app/views/fx_ios/release/locale_view.php
@@ -1,18 +1,19 @@
 <h1>Firefox for iOS Listing Copy (<?= $request['locale'] ?>)</h1>
+
 <h3>Title</h3>
-<pre><?= $app_title($translations) ?></pre>
+<pre <?= $direction ?>><?= $app_title($translations) ?></pre>
 
 <h3>Description</h3>
-<pre><?= $description($translations) ?></pre>
+<pre <?= $direction ?>><?= $description($translations) ?></pre>
 
 <h3>What’s new</h3>
-<pre><?= $whatsnew($translations) ?></pre>
+<pre <?= $direction ?>><?= $whatsnew($translations) ?></pre>
 
 <h3>Screenshots text</h3>
-<pre class="text-center"><?= $screenshots($translations) ?></pre>
+<pre <?= $direction ?> class="text-center"><?= $screenshots($translations) ?></pre>
 
 <h3>Keywords&mdash; <?= $keywords_warning ?></h3>
-<pre><?= $keywords($translations) ?></pre>
+<pre <?= $direction ?>><?= $keywords($translations) ?></pre>
 
 <h3>Other</h3>
-<pre><?= $other($translations) ?></pre>
+<pre <?= $direction ?>><?= $other($translations) ?></pre>
