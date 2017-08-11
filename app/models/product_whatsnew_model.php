b411deb8df0edb44bf352ce43e5cc8afcc78b365
Francesco Lodolo (:flod) <flodolo@mozilla.com>
2017-08-11 18:03:05 -0400

    (fixes #145) Include only complete locales in whatsnew view

--- app/models/product_whatsnew_model.php
+++ app/models/product_whatsnew_model.php
@@ -6,6 +6,33 @@ $channel = $request['channel'];
 $template_locales = $project->getStoreMozillaCommonLocales($product, $channel);
 $locales_mapping = $project->getLocalesMapping($project->getProductStore($product), true);
 
+// Check the list of locales with fully localized listing
+$listing_translated = [];
+foreach ($template_locales as $template_locale) {
+    // Consider en-US complete and move to next locale
+    if ($template_locale == 'en-US') {
+        $listing_translated[] = $template_locale;
+        continue;
+    }
+    $translations = new Translate(
+        $template_locale,
+        $project->getLangFiles($template_locale, $product, $channel, 'listing'),
+        LOCALES_PATH);
+    if ($translations->isFileTranslated()) {
+        // Include the current template
+        require TEMPLATES . $project->getTemplate($template_locale, $product, $channel);
+
+        // Check Play Store limits
+        $app_desc = $set_limit('google_description', $description($translations));
+        $app_title = $set_limit('google_title', $app_title($translations));
+        $short_desc = $set_limit('google_short_description', $short_desc($translations));
+
+        if (($app_desc + $app_title + $short_desc) == 3) {
+            $listing_translated[] = $template_locale;
+        }
+    }
+}
+
 $whatsnew_content = '';
 foreach ($template_locales as $template_locale) {
     $translations = new Translate(
@@ -13,7 +40,9 @@ foreach ($template_locales as $template_locale) {
         $project->getLangFiles($template_locale, $product, $channel, 'whatsnew'),
         LOCALES_PATH);
     // Include en-US and all translated locales
-    if ($translations->isFileTranslated() or $template_locale == 'en-US') {
+    $whatsnew_translated = ($template_locale == 'en-US') || $translations->isFileTranslated();
+
+    if ($whatsnew_translated && in_array($template_locale, $listing_translated)) {
         // Include the current template
         require TEMPLATES . $project->getTemplate($template_locale, $product, $channel);
 
