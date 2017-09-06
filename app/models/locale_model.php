a036e4ae8b8171819e2ea1f3fc11e7757f50480b
Francesco Lodolo [:flod] <flodolo@mozilla.com>
2017-09-06 08:04:11 +0200

    Clean up locale model
    Don't re-declare $set_limit, no need for multiple functions calls.

--- app/models/locale_model.php
+++ app/models/locale_model.php
@@ -9,7 +9,7 @@ $translations = new Translate($request['locale'], $lang_files, LOCALES_PATH);
 // Include the current template
 require TEMPLATES . $project->getTemplate($request['locale'], $request['product'], $request['channel']);
 
-$get_length = function ($string) {
+$show_length = function ($type, $string) use ($store_limits) {
     /*
         In locale view a lot of markup is added: the string is injected in a
         <span> with a style if untranslated, or with the original string as a
@@ -18,11 +18,8 @@ $get_length = function ($string) {
         maximum length.
     */
     $pattern = '/<span[^<]*?>([^<]*)<\/span>/im';
+    $length = mb_strlen(trim(preg_replace($pattern, '$1', $string)));
 
-    return mb_strlen(trim(preg_replace($pattern, '$1', $string)));
-};
-
-$set_limit = function ($type, $length) use ($store_limits) {
     $limit = $store_limits[$type];
     if ($length <= $limit) {
         return "{$length} characters";
@@ -35,22 +32,22 @@ $set_limit = function ($type, $length) use ($store_limits) {
 $store = $project->getProductStore($request['product']);
 if ($store == 'google') {
     $short_desc_warning = isset($short_desc)
-        ? $set_limit('google_short_description', $get_length($short_desc($translations)))
+        ? $show_length('google_short_description', $short_desc($translations))
         : '';
 
-    $listing_warning = $set_limit('google_description', $get_length($description($translations)));
-    $title_warning = $set_limit('google_title', $get_length($app_title($translations)));
+    $listing_warning = $show_length('google_description', $description($translations));
+    $title_warning = $show_length('google_title', $app_title($translations));
 
     if (in_array($request['channel'], $project->getProductChannels($request['product']))) {
         $whatsnew_warning = isset($whatsnew)
-            ? $set_limit('google_whatsnew', $get_length($whatsnew($translations)))
+            ? $show_length('google_whatsnew', $whatsnew($translations))
             : '';
     }
 }
 
 // Apple App Store also has lengths constraints
 if ($store == 'apple') {
-    $keywords_warning = $set_limit('apple_keywords', $get_length($keywords($translations)));
-    $title_warning = $set_limit('apple_title', $get_length($app_title($translations)));
-    $subtitle_warning = $set_limit('apple_subtitle', $get_length($app_subtitle($translations)));
+    $keywords_warning = $show_length('apple_keywords', $keywords($translations));
+    $title_warning = $show_length('apple_title', $app_title($translations));
+    $subtitle_warning = $show_length('apple_subtitle', $app_subtitle($translations));
 }
