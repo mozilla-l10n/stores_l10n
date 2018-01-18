488d5d7b5738c155ba4166e3077dc68bc599c1bb
Delphine Lebédel <lebedel.delphine@gmail.com>
2018-01-18 09:37:58 -0800

    Updated order for release

--- app/templates/fx_android/release/listing_apr_2016.php
+++ app/templates/fx_android/release/listing_apr_2016.php
@@ -102,9 +102,9 @@ $short_desc = function ($translations) use ($_) {
 
 $whatsnew = function ($translations) use ($_) {
     return <<<OUT
-* {$_('Removed the Firefox Search widget from home screen')}
 * {$_('Added the ability for users to add websites to home screen like native applications')}
 * {$_('Added support for FLAC (Free Lossless Audio Codec) playback')}
 * {$_('Added Bengali from Bangladesh (bn-BD) and Nepali (ne-NP) locales')}
+* {$_('Removed the Firefox Search widget from home screen')}
 OUT;
 };
