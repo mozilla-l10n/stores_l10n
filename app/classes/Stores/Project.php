d61d0f4981ac0367ba68ad83fc89e0f0d6c7c25f
Delphine Lebédel <lebedel.delphine@gmail.com>
2018-10-01 13:16:36 -0400

    Add What's New file for iOS 14 (#170)

--- app/classes/Stores/Project.php
+++ app/classes/Stores/Project.php
@@ -239,7 +239,7 @@ class Project
             'release' => [
                 'template' => 'fx_ios/release/listing_sept_2015.php',
                 'listing'  => 'fx_ios/description_release.lang',
-                'whatsnew' => 'fx_ios/whatsnew/ios_13.lang',
+                'whatsnew' => 'fx_ios/whatsnew/ios_14.lang',
             ],
         ],
         'focus_ios' => [
